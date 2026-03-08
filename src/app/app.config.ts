86d21567552a925a8f876ae7d497ee28f25c54f5
AnastasisP <51802248+AnastasisP@users.noreply.github.com>
2026-03-08 23:58:34 +0200

    feat: Refactor app structure and styles
    - Removed placeholder content from app.component.html and added a router outlet.
    - Created a new app.component.scss file for styles, replacing the previous styles.css.
    - Updated app.component.ts to include MatButtonModule for Angular Material support.
    - Enhanced app.config.ts to provide additional services and configurations.
    - Modified app.routes.ts to define application routes, including a default route.
    - Updated index.html to include new font styles and set the HTML class to "dark".
    - Implemented custom theming for Angular Material in styles.scss.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+import {
+  provideRouter,
+  withComponentInputBinding,
+} from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { appRoutes } from './app.routes';
+import { OrchestratorPort } from './core/ports/orchestrator.port';
+import { OrchestratorMockService } from './core/services/orchestrator.mock.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(appRoutes, withComponentInputBinding()),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    { provide: OrchestratorPort, useClass: OrchestratorMockService },
+  ],
 };
